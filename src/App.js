102caaeb06ebbdee0129e2406dddb344d751b16e
LakatosImre <lakatos.imre2002@gmail.com>
2022-12-07 14:26:06 +0100

    Created structure to folders, and site

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
 import './App.css';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Info from './components/info/Info';
+import Projects from './components/projects/Projects'
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <div className='content'>
+        <Info />
+        <Projects />
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+      <h1>Lakatos Imre</h1>
+      <p>Junior Developer</p>
+    </div>
+  )
+}
+
+export default Header
